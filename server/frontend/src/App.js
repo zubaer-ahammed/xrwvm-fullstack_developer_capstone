22897fa4298f55b563db27858a17073e7fae93ab
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2025-03-11 10:46:20 -0400

    added user management templates and views

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,13 @@
 import LoginPanel from "./components/Login/Login"
+import RegistrationPanel from "./components/Register/Register"
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegistrationPanel />} />
     </Routes>
   );
 }
