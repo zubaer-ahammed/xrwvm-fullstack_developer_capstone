3b914a15a6442e3213f122b10ec876a34e98dbd1
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2025-03-16 14:23:30 -0400

    CarMake and CarModel

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
+    
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
